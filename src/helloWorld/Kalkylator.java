4b3a349d8b24722638000231ea32192760b22962
fabian0011 <fabbemuson@gmail.com>
2022-10-31 11:44:25 +0100

    Added kalkylator and method add

---
+++ src/helloWorld/Kalkylator.java
@@ -0,0 +1,18 @@
+package helloWorld;
+
+public class Kalkylator {
+    private int first;
+    private int second;
+
+    public Kalkylator(int firstNumber, int secondNumber) {
+        first = firstNumber;
+        second = secondNumber;
+
+    }
+
+    public int add() {
+
+        return first = second;
+
+    }
+}
